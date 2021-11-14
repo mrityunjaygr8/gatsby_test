09dc9222caf17aa2872839ddc274fcb5f5c21eef
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-11-14 14:57:28 +0530

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>in gatsby tut</p>
+      <StaticImage
+        alt="Clifford"
+        src="../images/icon.png" />
     </Layout>
   )
 }
